748561fffc63b3f26f14df574e3d1f70a387e45a
mtalhajamil <talha.jamil@venturedive.com>
2018-10-19 11:44:12 +0500

    initial

---
+++ src/main/java/com/demo/mvnproject/MvnprojectApplication.java
@@ -0,0 +1,12 @@
+package com.demo.mvnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnprojectApplication.class, args);
+	}
+}
